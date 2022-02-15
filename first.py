6f0f3f5294717e3bb8a7a1843190ce8354a80739
thethoptergod <93736743+thethoptergod@users.noreply.github.com>
2022-02-14 19:36:46 -0500

    add stuff

---
+++ first.py
@@ -0,0 +1,5 @@
+print("Hello World!")
+print("How are you?")
+print("Create Hammer")
+print("Create Nails")
+print("Use Hammer on Nails")
---
+++ variables.py
@@ -0,0 +1,8 @@
+print("Variables")
+failed_subjects="6"
+name="John"
+print("Dear Mrs. Badger")
+print(name + " is failing " + failed_subjects + " subjects")
+print(name +  " will need to retake " + failed_subjects + " subjects")
+name = "Eric"
+print(name + " is doing well in math")
