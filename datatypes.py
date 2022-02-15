605e8f0c9627a466eb3d79543ff2e0310fa82fdf
thethoptergod <93736743+thethoptergod@users.noreply.github.com>
2022-02-15 05:22:20 -0500

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+from operator import truediv
+
+
+print("Datatypes")
+integer = 2
+float = 2.45
+boolean = True
+a = "it's"
+b='it\'s'
+print(type("Hello"))
+print(type(1))
+print(type(1.64))
+print(type(True))
+print(str(integer))
+print(integer)
